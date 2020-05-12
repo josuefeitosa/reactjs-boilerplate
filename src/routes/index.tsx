860c4cec1fb64768e04b63349f993cb158f9b8a9
Josué Feitosa <josue.feitosa@outlook.com.br>
2020-05-12 16:51:26 -0300

    Implementação do boilerplate padrão

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Login from '../pages/Login';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Login}/>
+    </Switch>
+  )
+}
+
+export default Routes;
